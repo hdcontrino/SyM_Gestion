74ce11622a95aee555654e65f04425becd747d63
Daniel <danielcontrino@icloud.com>
2025-03-06 17:42:26 +0000

    Storytime final

--- customs/index.php
+++ customs/index.php
@@ -12,12 +12,32 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+    </style>
 </head>
 
 <body>
     <section>
         <?php include('rosco.html'); ?>
     </section>
+
+    <div style="height: 200px;"></div>
+    <hr>
+    <div style="height: 200px;"></div>
+
+    <section>
+        <?php include('storytime.html'); ?>
+    </section>
+
+    <div style="height: 200px;"></div>
+    <hr>
+    <div style="height: 200px;"></div>
+
 </body>
 
 </html>
